beb3348dba2932f4853a84fe54c622bace5b3426
Sabilla Luthfi Rahmadhan <mamadhan017@gmail.com>
2023-05-14 15:04:39 +0700

    try export import

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-console.log("Menyalakan mesin kopi");
-console.log("Menggiling biji kopi");
-console.log("Memanaskan air");
-console.log("Mencampurkan air dan kopi");
-console.log("Menuangkan kopi ke dalam gelas");
-console.log("Menuangkan susu ke dalam gelas");
-console.log("Kopi Anda sudah siap!");
+import { coffeeStock as stock, isCoffeeMachineReady } from './state.js';
+ 
+// const makeCoffee = (type, miligrams) => {
+//     if (coffeeStock[type] >= miligrams) {
+//       console.log("Kopi berhasil dibuat!");
+//     } else {
+//       console.log("Biji kopi habis!");
+//     }
+//   }
+   
+//   makeCoffee("robusta", 80);
+
+console.log(stock);
+console.log(isCoffeeMachineReady);
 
