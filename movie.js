ce5b1a424d7c31e317588243b393408c2ae8c9f6
Sabilla Luthfi Rahmadhan <mamadhan017@gmail.com>
2023-05-18 20:13:35 +0700

    try Asynchronous Handling

--- movie.js
+++ movie.js
@@ -1,54 +1,21 @@
-function withDrawMoney(amount) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (amount > 100) {
-          reject(new Error('Not enough money to withdraw'));
-        }
-  
-        resolve(amount);
-      }, 1000);
-    });
+import { withDrawMoney, buyCinemaTicket, goInsideCinema } from './utils.js';
+
+async function watchMovie(amount) {
+  try {
+    const money = await withDrawMoney(amount);
+    const ticket = await buyCinemaTicket(money);
+    const result = await goInsideCinema(ticket);
+
+    return result;
+  } catch (error) {
+    throw error;
   }
-  
-  function buyCinemaTicket(money) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (money < 10) {
-          reject(new Error('not enough money to buy ticket'));
-        }
-  
-        resolve('ticket-1');
-      }, 1000);
-    });
-  }
-  
-  function goInsideCinema(ticket) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (!ticket) {
-          reject(new Error('no ticket'));
-        }
-  
-        resolve('enjoy the movie');
-      }, 1000);
-    });
-  }
-  
-  function watchMovie() {
-    withDrawMoney(10)
-      .then((money) => {
-        return buyCinemaTicket(money);
-      })
-      .then((ticket) => {
-        return goInsideCinema(ticket);
-      })
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-  }
-  
-  watchMovie();
-  
+}
+
+watchMovie(10)
+  .then((result) => console.log(result)) // enjoy the movie
+  .catch((error) => console.log(error.message));
+
+watchMovie(5)
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error.message)); // not enough money to buy ticket
